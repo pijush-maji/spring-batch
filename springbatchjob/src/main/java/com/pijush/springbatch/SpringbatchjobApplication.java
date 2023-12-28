cad90228015b535771d2a2d1a99a70b055d8eb89
pijush-maji <pijushmaji08@gmail.com>
2023-12-28 23:03:51 +0530

    Async feature added

--- springbatchjob/src/main/java/com/pijush/springbatch/SpringbatchjobApplication.java
+++ springbatchjob/src/main/java/com/pijush/springbatch/SpringbatchjobApplication.java
@@ -2,8 +2,10 @@ package com.pijush.springbatch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class SpringbatchjobApplication {
 
 	public static void main(String[] args) {
---
+++ springbatchjob/src/main/java/com/pijush/springbatch/service/JobService.java
@@ -0,0 +1,58 @@
+package com.pijush.springbatch.service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JobService {
+	
+	@Autowired
+	private JobLauncher jobLauncher;
+
+	@Qualifier("firstJob")
+	@Autowired
+	private Job firstJob;
+
+	@Qualifier("secondJob")
+	@Autowired
+	private Job secondJob;
+
+	@Async
+	public void runJob(String jobName) {
+		// Way 1 to create jobParameters
+		JobParameters jobParameters1 = new JobParametersBuilder().addLong("Start at - ", System.currentTimeMillis())
+				.toJobParameters();
+
+		// Way 2 to create jobParameters
+		Map<String, JobParameter<?>> params = new HashMap<>();
+		params.put("Started at", new JobParameter<Long>(System.currentTimeMillis(), Long.class));
+
+		JobParameters jobParameters2 = new JobParameters(params);
+		
+		JobExecution jobExecution=null;
+
+		try {
+
+			if (jobName.equals("FirstJob")) {
+				jobExecution = jobLauncher.run(firstJob, jobParameters1);
+			} else if (jobName.equals("SecondJob")) {
+				jobExecution = jobLauncher.run(secondJob, jobParameters2);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("Job Execution Id - "+jobExecution.getId());
+	}
+
+}
